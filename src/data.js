53d01aabffc111ba58ccbf1345f76ea55964f925
Juan Soledad <soledad.charly@hotmail.com>
2024-04-09 14:10:49 -0600

    base code

---
+++ src/data.js
@@ -0,0 +1,263 @@
+export const curriculum = {
+	experience: [
+		{
+			occupation: "Full Stack Developer",
+			site: "DevelopMX",
+			time: "03 2023 - Actually...",
+      description: "",
+      projects: [
+      ],
+			tasks: [
+				"• Analizar la lógica de negocio para generar procedimientos almacenados que se ejecuten correctamente diariamente, utilizando SQL como lenguaje en Azure.",
+				"• Revisando arquitectura de sistema para implementar nuevas funcionalidades analizando el workflow de código para estimar tiempos de entrega. ",
+				"• Recuperando tickets generados para mejorar funcionalidades, modificando los endpoints utilizados y generando la documentación necesaria para testing.",
+				"• Analizar el código para utilizar las mejores prácticas, corregir algunas funciones de obtención de datos utilizando TDD para probar la recuperación de datos.",
+				"• Probar funcionalidades de la plataforma para mejorar el diseño, corrigiendo algunos componentes con estilos y datos mostrados.",
+			],
+		},
+		{
+			occupation: "Full Stack Developer",
+			site: "Freelancer",
+			time: "01 2021 - Actually...",
+      description: "",
+      projects: [
+        {
+          title: 'Sistema de gestión de clínicas radiológicas',
+          description: "",
+          tasks: [
+            "• Estructura, planificación de la aplicación, esquema de la base de datos y diseño de la interfaz.",
+            "• Creación del backend con Django, habilitar la comunicación API con autenticación de usuario multinivel.",
+            "• Desarrollo del frontend usando Vite + Vue 3 con llamadas al backend a través de la API creada para mostrar datos de geolocalización a mapeo, alguna visualización de datos de clientes, ingresos y consultas realizadas."
+          ]
+        },
+        {
+          title: 'SEI Website',
+          description: "",
+          tasks: [
+            "o	Página web sencilla para mostrar catálogos de material eléctrico utilizando HTML vainilla, CSS3 y JavaScript.",
+          ]
+        }
+      ],
+			tasks: [],
+    },
+    {
+			occupation: "Developer, Robotics Teacher",
+			site: "Robot Punk",
+			time: "06 2016 - 07 2019",
+      description: "",
+      projects: [
+      ],
+			tasks: [
+				"•	Implementación de nuevas funcionalidades para la plataforma interna, desarrollo de nuevas vistas para el control del alumnado."
+        ,"•	Mantenimiento de ordenadores, redes de internet."
+        ,"•	Desarrollo de sistema de puntuación para competiciones utilizando Java y Arduino y plataformas web."
+        ,"•	Planificación de temas a impartir durante las sesiones."
+        ,"•	Desarrollo de planeaciones para impartir durante los semestres para los diferentes cursos de Vex y Lego. "
+        ,"•	Enseñanza sobre construcción y programación de robots distribuidos en diferentes niveles. ",
+        "•	Enseñanza de temas avanzados para equipos de competencias enfocados a programación y control de mecanismos."
+			],
+		},
+  ],
+  education: [
+    {
+      name: "UTEL - Universidad en Linea",
+      career: "Computer Science Engineering",
+      gradYear: "2025",
+      projects:[],
+    },
+    {
+      name: "CBTIS No. 159 'Dr. Belisario Dominguez Plasencia'",
+      career: "Programming Associate",
+      gradYear: "2019",
+      projects: [
+        {
+          title: "CASN - Control and Access System Notification",
+          description:"Sistema para permitir el acceso a la escuela mediante tarjeta RFID o huella dactilar, enviando el registro a una base de datos para registrar el acceso, con una aplicación el profesor puede pasar lista y si el alumno no se registra la entrada marca automáticamente la inasistencia (podría modificarse después de clase como retraso)."
+        },
+        {
+          title: "E-Learning Robotics Platform",
+          description:"Plataforma para enseñar sobre robótica utilizando kits de robótica como LEGO o VEX, separados por cursos o lecciones interactivas para potenciar el aprendizaje cognitivo y la simplificación de mecanismos."
+        },
+        {
+          title: "Host Server Template",
+          description:"Plantilla para utilizar en archivo de servidor para realizar la configuración del localhost como como WampServer o XAMPP, separar todas las configuraciones en una vista simple, muestra todos los archivos utilizando árbol de exploración con iconos a todos los archivos y subcarpetas."
+        },
+      ],
+    },
+  ]
+};
+
+export const skills = [
+  {
+    type: 'framework web',
+    technology: "django",
+    icon: "vscode-icons:file-type-django",
+    site: 'https://www.djangoproject.com/'
+  },
+  {
+    type: 'framework web',
+    technology: "reactjs",
+    icon: "vscode-icons:file-type-reactjs",
+    site: 'https://react.dev/'
+  },
+  {
+    type: 'framework web',
+    technology: "angular",
+    icon: "vscode-icons:file-type-angular",
+    site: 'https://angular.io/'
+  },
+  {
+    type: 'framework web',
+    technology: "vuejs",
+    icon: "vscode-icons:file-type-vue",
+    site: 'https://vuejs.org/'
+  },
+  {
+    type: 'framework web',
+    technology: "astro",
+    icon: "vscode-icons:file-type-astro",
+    site: 'https://astro.build/'
+  },
+  {
+    type: 'framework web',
+    technology: "meteor",
+    icon: "vscode-icons:file-type-meteor",
+    site: 'https://www.meteor.com/'
+  },
+  {
+    type: 'framework web',
+    technology: "rails",
+    icon: "vscode-icons:file-type-rails",
+    site: 'https://rubyonrails.org/'
+  },
+  {
+    type: 'language',
+    technology: "javascript",
+    icon: "vscode-icons:file-type-js",
+    site: 'https://developer.mozilla.org/es/docs/Web/JavaScript'
+  },
+  {
+    type: 'language',
+    technology: "php",
+    icon: "vscode-icons:file-type-php3",
+    site: 'https://www.php.net/'
+  },
+  {
+    type: 'language',
+    technology: "handlebars",
+    icon: "vscode-icons:file-type-handlebars",
+    site: 'https://handlebarsjs.com/'
+  },
+  {
+    type: 'language',
+    technology: "ruby",
+    icon: "vscode-icons:file-type-ruby",
+    site: 'https://www.ruby-lang.org/'
+  },
+  {
+    type: 'language',
+    technology: "c",
+    icon: "vscode-icons:file-type-c",
+    site: 'https://www.djangoproject.com/'
+  },
+  {
+    type: 'language',
+    technology: "cpp",
+    icon: "vscode-icons:file-type-cpp",
+    site: 'https://www.djangoproject.com/'
+  },
+  {
+    type: 'language',
+    technology: "csharp",
+    icon: "vscode-icons:file-type-csharp",
+    site: 'https://dotnet.microsoft.com/en-us/languages/csharp'
+  },
+  {
+    type: 'language',
+    technology: "typescript",
+    icon: "vscode-icons:file-type-typescript",
+    site: 'https://www.typescriptlang.org/'
+  },
+  {
+    type: 'language',
+    technology: "python",
+    icon: "vscode-icons:file-type-python",
+    site: 'https://www.python.org/'
+  },
+  {
+    type: 'language',
+    technology: "java",
+    icon: "devicon:java",
+    site: 'https://www.java.com/es/'
+  },
+  {
+    type: 'language',
+    technology: "dart",
+    icon: "vscode-icons:file-type-dartlang",
+    site: 'https://dart.dev/'
+  },
+  {
+    type: 'language',
+    technology: "kotlin",
+    icon: "vscode-icons:file-type-kotlin",
+    site: 'https://kotlinlang.org/'
+  },
+  {
+    type: 'preprocessor',
+    technology: "sass",
+    icon: "devicon:sass",
+    site: 'https://sass-lang.com/'
+  },
+  {
+    type: 'preprocessor',
+    technology: "bootstrap",
+    icon: "devicon:bootstrap",
+    site: 'https://getbootstrap.com/'
+  },
+  {
+    type: 'database',
+    technology: "mongodb",
+    icon: "devicon:mongodb",
+    site: 'https://www.mongodb.com/'
+  },
+  {
+    type: 'database',
+    technology: "sql",
+    icon: "devicon:mysql",
+    site: 'https://www.mysql.com/'
+  },
+  {
+    type: 'database',
+    technology: "azure",
+    icon: "vscode-icons:file-type-azure",
+    site: 'https://azure.microsoft.com/'
+  },
+]
+
+export const projects = [
+  {
+    name: 'CASN - Control Acceso y Notificacion',
+    image: 'casn.png',
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis velit vero quo corporis, nostrum ullam ratione quibusdam quisquam in. Quos vero rerum sapiente a voluptate eius quia, similique hic ipsum."
+  },
+  {
+    name: 'E-Learning Robotics platform',
+    image: 'casn.png',
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis velit vero quo corporis, nostrum ullam ratione quibusdam quisquam in. Quos vero rerum sapiente a voluptate eius quia, similique hic ipsum."
+  },
+  {
+    name: 'Host Server Template',
+    image: 'casn.png',
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis velit vero quo corporis, nostrum ullam ratione quibusdam quisquam in. Quos vero rerum sapiente a voluptate eius quia, similique hic ipsum."
+  },
+  {
+    name: 'RadioClinic',
+    image: 'casn.png',
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis velit vero quo corporis, nostrum ullam ratione quibusdam quisquam in. Quos vero rerum sapiente a voluptate eius quia, similique hic ipsum."
+  },
+  {
+    name: 'SEI Website',
+    image: 'casn.png',
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis velit vero quo corporis, nostrum ullam ratione quibusdam quisquam in. Quos vero rerum sapiente a voluptate eius quia, similique hic ipsum."
+  },
+]
---
+++ tailwind.config.mjs
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}','./node_modules/flowbite/**/*.js'],
+	theme: {
+		extend: {
+			colors: {
+				'chs-blue': '#080D16'
+			}
+		},
+	},
+	darkMode: 'class',
+	plugins: [
+		require('flowbite/plugin')
+	],
+}

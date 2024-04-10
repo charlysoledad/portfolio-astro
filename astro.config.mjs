aa0a7d661a1a2bf2e8de43d4a460646de9ac3d20
Juan Soledad <soledad.charly@hotmail.com>
2024-04-09 22:52:19 -0600

    adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,12 @@ import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
 
 import netlify from "@astrojs/netlify";
+import * as path from 'path';
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
   integrations: [tailwind(), icon()],
-  adapter: netlify()
+  adapter: netlify(),
+  
 });

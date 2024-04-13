e809173f74a04c110d9ce7f1b0e5be591cba405e
Juan Soledad <soledad.charly@hotmail.com>
2024-04-13 15:19:57 -0600

    adapter changed to vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,16 +2,17 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
 
-import netlify from "@astrojs/netlify";
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
   integrations: [tailwind(), icon()],
-  adapter: netlify(),
+  adapter: vercel(),
   image: {
     domains: ["astro.build"],
-    remotePatterns: [{ protocol: "https" }],
-  },
-
+    remotePatterns: [{
+      protocol: "https"
+    }]
+  }
 });
